d4701a49dafd93e9bd551256fa5026bbc2448b32
xondamir404 <xondamir.qarshiboyev404@gmail.com>
2026-03-23 04:39:20 -0400

    Initial commit -MAC changer tool

---
+++ mac_changerr.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+
+from nltk.inference.prover9 import arguments
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest= "interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest= "new_mac", help="New MAC address")
+
+(options, arguments) = parser.parse_args()
+
+
+interface = options.interface
+
+new_mac = options.new_mac
+
+print("[+] CHanging MAC address for " + interface + " to " + new_mac)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
